b8f82ec82bf05cb53b357efd975c9f6b9977e306
Jacob Mortelliti <jacob.harrowmortelliti@maine.eu>
2016-06-10 18:57:05 -0400

    Made a quote appear when the pages starts.

--- myScript.js
+++ myScript.js
@@ -1,10 +1,32 @@
 var clicks = 0;
+
 $(function() {
   twttr.ready(
     function (twttr) {
-$('#new-quote').on('click', function(e) {
-    e.preventDefault();
-    $.ajax( {
+
+$.ajax( {
+      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      success: function(data) {
+        var post = data.shift(); // The data is an array of posts. Grab the first one.
+        var author = post.title;
+        var quoteContent = $(post.content).text();
+        $('#quote-title').text(post.title);
+        $('#quote-content').html(post.content);
+        twttr.widgets.createShareButton(
+          "",
+          document.getElementById("tweet-container"),
+          {
+            size: "large",
+            text: quoteContent + " - "+ author,
+          }
+        );
+      },
+      cache: false
+});
+
+$('#new-quote').on('click', function(e){
+  e.preventDefault();
+  $.ajax( {
       url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
       success: function(data) {
         var post = data.shift(); // The data is an array of posts. Grab the first one.
@@ -23,13 +45,9 @@ $('#new-quote').on('click', function(e) {
             text: quoteContent + " - "+ author,
           }
         );
-
-         
       },
       cache: false
+});
+});
     });
-  });
-  }
-);
-
 });
